f022e0482a3dea62be89ded454c66e4c1614438a
Nadiabenhessine <nadiabenhessine@gmail.com>
2021-10-17 19:47:29 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import GetAPI from './GetAPI';
+import PostAPI from './PostAPI';
+
+function App() {
+  return (
+    <div>
+      <GetAPI />  
+      <PostAPI />
+    </div>
+  );
+} 
+
+export default App;
